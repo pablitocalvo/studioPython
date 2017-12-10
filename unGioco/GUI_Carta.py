f4cde68992640d4c0b5f4a0f7ae6571e94ce9837
paolo <paolo@NBpaolo.lan>
2017-12-10 23:16:43 +0100

    aggiunta GUI_Carta e immagini

--- unGioco/GUI_Carta.py
+++ unGioco/GUI_Carta.py
@@ -1,16 +1,30 @@
-from PyQt4 import QtGui
-from PyQt4.QtCore import QObject, pyqtSlot
+from PyQt4 import QtGui,QtCore
+from PyQt4.QtCore import pyqtSlot,QObject
+
 from Carta import Carta
 
 
 class GUI_Carta(QtGui.QPushButton, Carta):
-	def __init__(self):
+	
+	def __init__(self,  valore ):
+		
+		Carta.__init__(self,valore)
+				
 		QtGui.QPushButton.__init__(self)
 		self.setFixedWidth(50)
-		Carta.__init__(self,"A")
+		self.setFixedHeight(50)
+		
+		self.setStyleSheet("background: url(img/dorso.png); ")
+		
+	def viene_scoperta(self):
+		Carta.viene_scoperta(self) 
+		self.setStyleSheet("background: url(img/"+self.get_valore()+".png); ")
 
-	@pyqtSlot()		
-	def onclick(self):
-		print "cliccato"
-		self.setStyleSheet("background-image: url(prova.png);")
-		return 0
+	def viene_coperta(self):
+		Carta.viene_coperta(self)
+		self.setStyleSheet("background: url(img/dorso.png); ")
+		
+	def viene_tolta_dal_tavolo(self):
+		Carta.viene_tolta_dal_tavolo(self)
+		self.setStyleSheet("background: url(img/none.png); ")
+		
--- unGioco/GUI_main.py
+++ unGioco/GUI_main.py
@@ -1,43 +1,85 @@
 
 import sys
 from PyQt4 import QtGui, QtCore # importiamo i moduli necessari
-from PyQt4.QtCore import QObject, pyqtSlot
+from PyQt4.QtCore import QObject,pyqtSlot
+from PyQt4.QtGui import QGridLayout, QWidget,QPushButton
 
 
-from Carta import Carta
+from GUI_Carta import GUI_Carta
 
 
 class MainWindow(QtGui.QMainWindow):
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self) 
-		# self.resize(350, 250) # ridimensiona la finestra
+		self.resize(350, 250) # ridimensiona la finestra
 		self.setWindowTitle('MainWindow')
 		# self.statusBar().showMessage('Messaggio') # crea una veloce barra di stato
 	
-	@pyqtSlot()
-	def cartaCliccata (self):
-		print "cliccataaa"
+class GUI_Partita( QtGui.QWidget ) :
+	def __init__(self, window ) :
+		QWidget.__init__(self)
 		
-class GUI_Carta(QtGui.QPushButton, Carta):
-	def __init__(self):
-		QtGui.QPushButton.__init__(self)
-		Carta.__init__(self,"A")
-
+		grid = QGridLayout( self )
+		c=0
+		for valore in ["A","B","C","D","E","A","B","C","D","E"] :
+			
+			gc = GUI_Carta(valore)
+			self.connect( gc, QtCore.SIGNAL('clicked()'), QtCore.SLOT('onCartaCliccata()'))
+			
+			grid.addWidget( gc, c / 5  , c%5)
+			c=c+1
+		window.setCentralWidget ( self )
+	
+		self.carte_scoperte=0
+		self.numero_carte_sul_tavolo=10
+		
+		
 	@pyqtSlot()		
-	def onclick(self):
-		print "cliccato"
-		self.setStyleSheet("background-image: url(prova.png);")
-		return 0
+	def onCartaCliccata(self):
+		carta_cliccata = self.sender()
+		
+		print self.sender().get_valore()
+		
+		if carta_cliccata.is_fuori() :
+			return
+		if carta_cliccata.is_scoperta():
+			return
+		
+		carta_cliccata.viene_scoperta();
+		if  self.carte_scoperte == 0 :
+			
+			self.prima_carta = carta_cliccata
+			self.carte_scoperte = 1
+			
+		elif self.carte_scoperte == 1 :
+		
+			if carta_cliccata.is_uguale_a ( self.prima_carta ):
+				self.prima_carta.viene_tolta_dal_tavolo()
+				carta_cliccata.viene_tolta_dal_tavolo()
+				self.carte_scoperte=0
+			else :
+				self.seconda_carta = carta_cliccata
+				self.carte_scoperte = 2 
+		
+		elif self.carte_scoperte == 2 :
+			self.prima_carta.viene_coperta()
+			self.seconda_carta.viene_coperta()
+			
+			self.prima_carta = carta_cliccata
+			self.carte_scoperte = 1
+			
+			
+	
+		
+		
+
 
 app = QtGui.QApplication(sys.argv)
 
 main = MainWindow()
 
-gcarta=GUI_Carta()
+partita = GUI_Partita( main )
 
-gcarta.connect(gcarta, QtCore.SIGNAL('clicked()'), QtCore.SLOT('onclick()'));
-
-main.setCentralWidget(gcarta)
 
 main.show()
 
